93d7fe6f7e02724d82f126eedcd1eecfd8ef58d7
Administrator <Administrator@192.168.1.105>
2017-12-21 21:27:46 +0800

    new data

---
+++ sql/data/supply/sci/dianping-company-data-lego.sql
@@ -0,0 +1,22 @@
+/*
+SQLyog Community v12.5.0 (64 bit)
+MySQL - 10.1.28-MariaDB 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+
+insert into `destoon_company_data` (`userid`, `content`) values('520','');
+insert into `destoon_company_data` (`userid`, `content`) values('519','');
+insert into `destoon_company_data` (`userid`, `content`) values('518','');
+insert into `destoon_company_data` (`userid`, `content`) values('517','');
+insert into `destoon_company_data` (`userid`, `content`) values('516','');
+insert into `destoon_company_data` (`userid`, `content`) values('515','');
+insert into `destoon_company_data` (`userid`, `content`) values('514','');
+insert into `destoon_company_data` (`userid`, `content`) values('513','');
+insert into `destoon_company_data` (`userid`, `content`) values('512','');
+insert into `destoon_company_data` (`userid`, `content`) values('511','');
+insert into `destoon_company_data` (`userid`, `content`) values('510','');
+insert into `destoon_company_data` (`userid`, `content`) values('509','');
+insert into `destoon_company_data` (`userid`, `content`) values('507','');
+insert into `destoon_company_data` (`userid`, `content`) values('508','');
---
+++ sql/data/supply/sci/dianping-sellsearch-lego.sql
@@ -0,0 +1,21 @@
+/*
+SQLyog Community v12.5.0 (64 bit)
+MySQL - 10.1.28-MariaDB 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+insert into `destoon_sell_search_5` (`itemid`, `catid`, `areaid`, `content`, `status`, `sorttime`) values('1385','102','1','基础junior1智能系列,供应,科学,乐高','3','1513786874');
+insert into `destoon_sell_search_5` (`itemid`, `catid`, `areaid`, `content`, `status`, `sorttime`) values('1386','102','1','乐高课程,供应,科学,乐高','3','1513786874');
+insert into `destoon_sell_search_5` (`itemid`, `catid`, `areaid`, `content`, `status`, `sorttime`) values('1387','102','1','机器人等级考试认证,供应,科学,乐高','3','1513786874');
+insert into `destoon_sell_search_5` (`itemid`, `catid`, `areaid`, `content`, `status`, `sorttime`) values('1388','102','1','乐高机器人冬令营寒假托管班,供应,科学,乐高','3','1513786874');
+insert into `destoon_sell_search_5` (`itemid`, `catid`, `areaid`, `content`, `status`, `sorttime`) values('1389','102','1','精品机器人能力训练课程AY2,供应,科学,乐高','3','1513786874');
+insert into `destoon_sell_search_5` (`itemid`, `catid`, `areaid`, `content`, `status`, `sorttime`) values('1390','102','1','Python编程趣味课程,供应,科学,乐高','3','1513786874');
+insert into `destoon_sell_search_5` (`itemid`, `catid`, `areaid`, `content`, `status`, `sorttime`) values('1391','102','1','3+ 游戏与规则,供应,科学,乐高','3','1513786874');
+insert into `destoon_sell_search_5` (`itemid`, `catid`, `areaid`, `content`, `status`, `sorttime`) values('1392','102','1','编程猫趣味课,供应,科学,乐高','3','1513786874');
+insert into `destoon_sell_search_5` (`itemid`, `catid`, `areaid`, `content`, `status`, `sorttime`) values('1393','102','1','3-4岁课程包,供应,科学,乐高','3','1513786874');
+insert into `destoon_sell_search_5` (`itemid`, `catid`, `areaid`, `content`, `status`, `sorttime`) values('1394','102','1','能力训练课程AY课程,供应,科学,乐高','3','1513786875');
+insert into `destoon_sell_search_5` (`itemid`, `catid`, `areaid`, `content`, `status`, `sorttime`) values('1395','102','1','机器人等级考试认证,供应,科学,乐高','3','1513786875');
+insert into `destoon_sell_search_5` (`itemid`, `catid`, `areaid`, `content`, `status`, `sorttime`) values('1396','102','1','亲子挑战赛,供应,科学,乐高','3','1513786875');
+insert into `destoon_sell_search_5` (`itemid`, `catid`, `areaid`, `content`, `status`, `sorttime`) values('1397','102','1','乐高机器人课48课时包,供应,科学,乐高','3','1513786875');
+insert into `destoon_sell_search_5` (`itemid`, `catid`, `areaid`, `content`, `status`, `sorttime`) values('1398','102','1','4-8岁积木机器人课程,供应,科学,乐高','3','1513786875');
