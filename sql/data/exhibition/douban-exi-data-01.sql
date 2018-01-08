f8d82f1f4e0c86e7cd01b7702410c4a5502e856b
Administrator <Administrator@192.168.1.105>
2018-01-08 17:35:28 +0800

    new data

---
+++ sql/data/exhibition/douban-exi-data-01.sql
@@ -0,0 +1,13 @@
+/*
+SQLyog Community v12.5.0 (64 bit)
+MySQL - 10.1.28-MariaDB 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+insert into `destoon_exhibit_data` (`itemid`, `content`) values('216','<h2>活动须知</h2>\n    <div class=\"wr\">\n        时间地点：<br/>1月13日（周六）下午13：30——15：00万柳中路6号院3号楼圣贝牙科<br/>1月14日（周日）下午13：30——15：00南磨房路甲2号楼圣贝牙科<br/><br/><br/>招募人群：5-12岁儿童（尤其是有咬手指、口呼吸、吐舌头习惯，牙齿不齐、牙列拥挤或稀释、地包天等情况的小朋友）<br/>招募人数：30组家庭<br/>活动地点：北京圣贝口腔儿童活动室<br/>活动时间：<br/>1月13日（周六）下午13：30——15：00万柳中路6号院3号楼圣贝牙科<br/>1月14日（周日）下午13：30——15：00南磨房路甲2号楼圣贝牙科<br/>活动流程：<br/>1.到院签到，写下你的“雪人计划”<br/>2.由口腔医师讲解儿童口腔早期干预知识<br/>3.亲子互动环节：现场堆雪人<br/><br/>关于雪：北京没下雪，我们提供与真雪手感无异的人造雪。');
+insert into `destoon_exhibit_data` (`itemid`, `content`) values('217','<h2>附近热门活动（距离本活动3公里范围内）</h2>\n      \n  <ul class=\"gallery gallery-pic115\">\n      <li>\n        <div class=\"pic\">\n          <a tabindex=\"-1\" href=\"https://www.douban.com/event/29634300/\" title=\"不忘來時路——中國百年鞋履展\">\n            <img src=\"https://img3.doubanio.com/view/event_poster/large/public/bded5498060f676.jpg\" alt=\"\" />\n          </a>');
+insert into `destoon_exhibit_data` (`itemid`, `content`) values('218','<h2>附近热门活动（距离本活动3公里范围内）</h2>\n      \n  <ul class=\"gallery gallery-pic115\">\n      <li>\n        <div class=\"pic\">\n          <a tabindex=\"-1\" href=\"https://www.douban.com/event/29341931/\" title=\"【周末精品】❤ ❤ ❤【零基础】英语学习 ❤ ❤ ❤\">\n            <img src=\"https://img1.doubanio.com/view/event_poster/large/public/0d2f0826c2d3b19.jpg\" alt=\"\" />\n          </a>');
+insert into `destoon_exhibit_data` (`itemid`, `content`) values('219','<h2>活动须知</h2>\n    <div class=\"wr\">\n        京承高速（或机场高速、机场二高速）转 京平高速，木燕路出口出来，遇红绿灯左转，北行8公里，见“湿地停车场”路牌后左转，沿路行驶1000米，右手边见公园南门（内有一石拱桥），致电俱乐部报备车牌号码后，右转进入，两边树下停车位免费停车，继续步行50米至俱乐部。（问路电话61410108） 注意事项： 活动全程禁止吸烟和一切野外用火 湿地公园的其他公共区域属防火闭园期，不得进入游览 活动只接待提前预订并支付成功的客人。 如遇天气变化（包括严重雾霾），俱乐部可适当调整课程流程和内容以更好地适应天气。如遇极端天气，俱乐部将最晚于活动当天通知大家取消活动。 不要跨越冰上警戒带，到非体验场地游玩，上冰前务必穿好救生衣。 现场禁止食用带果壳类零食（如花生，瓜子）。 请大家爱护环境和设备设施，如有损坏须照价赔偿。 冰上寒冷，建议着防滑雪地靴、防水防风保暖衣裤、帽子、手套。 冰钓的主要对象鱼为鲫鱼和鲤鱼，冰钓上鱼概率会比夏天钓鱼低，活动现场有专人提供咨询和指导，也可先网上搜集预习一下“冰钓技巧”。');
+insert into `destoon_exhibit_data` (`itemid`, `content`) values('220','<h2>活动须知</h2>\n    <div class=\"wr\">\n        请孩子自备手套、宽松的运动裤、运动鞋');
+insert into `destoon_exhibit_data` (`itemid`, `content`) values('221','<h2>活动须知</h2>\n    <div class=\"wr\">\n        仅需99元就可参加价值2999元的精品课程<br/><br/>导师解读家族树  （原价999元）<br/><br/>免费个案机会  （原价2000元）<br/><br/>报名联系助理daotwh (微信) 赠送海灵格经典家排书籍一本<br/><br/>课程现场将抽取大奖赠送2018年道同文化会员金卡，可享受免费咨询一次（价值1200元），报名课程9.5折优惠');
