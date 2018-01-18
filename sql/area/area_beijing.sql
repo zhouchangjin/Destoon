baff75100786a6b1e829aa596a11e40e2b8b9fef
Administrator <Administrator@192.168.1.105>
2018-01-18 14:54:03 +0800

    update area.sql

--- sql/area/area_beijing.sql
+++ sql/area/area_beijing.sql
@@ -1,20 +1,24 @@
 /*
-SQLyog v10.2 
-MySQL - 5.0.27-community-nt 
+SQLyog Community v12.5.0 (64 bit)
+MySQL - 10.1.28-MariaDB 
 *********************************************************************
 */
 /*!40101 SET NAMES utf8 */;
 
-insert into `destoon_area` (`areaid`, `areaname`, `parentid`, `arrparentid`, `child`, `arrchildid`, `listorder`) values('1','北京','0','0','1','1,392,393,394,395,396,397,398,399,400,401,402,403','1');
-insert into `destoon_area` (`areaid`, `areaname`, `parentid`, `arrparentid`, `child`, `arrchildid`, `listorder`) values('392','海淀区','1','0,1','0','392','392');
-insert into `destoon_area` (`areaid`, `areaname`, `parentid`, `arrparentid`, `child`, `arrchildid`, `listorder`) values('393','朝阳区','1','0,1','0','393','393');
-insert into `destoon_area` (`areaid`, `areaname`, `parentid`, `arrparentid`, `child`, `arrchildid`, `listorder`) values('394','东城区','1','0,1','0','394','394');
-insert into `destoon_area` (`areaid`, `areaname`, `parentid`, `arrparentid`, `child`, `arrchildid`, `listorder`) values('395','西城区','1','0,1','0','395','395');
-insert into `destoon_area` (`areaid`, `areaname`, `parentid`, `arrparentid`, `child`, `arrchildid`, `listorder`) values('396','丰台区','1','0,1','0','396','396');
-insert into `destoon_area` (`areaid`, `areaname`, `parentid`, `arrparentid`, `child`, `arrchildid`, `listorder`) values('397','石景山区','1','0,1','0','397','397');
-insert into `destoon_area` (`areaid`, `areaname`, `parentid`, `arrparentid`, `child`, `arrchildid`, `listorder`) values('398','房山区','1','0,1','0','398','398');
-insert into `destoon_area` (`areaid`, `areaname`, `parentid`, `arrparentid`, `child`, `arrchildid`, `listorder`) values('399','通州区','1','0,1','0','399','399');
-insert into `destoon_area` (`areaid`, `areaname`, `parentid`, `arrparentid`, `child`, `arrchildid`, `listorder`) values('400','顺义区','1','0,1','0','400','400');
-insert into `destoon_area` (`areaid`, `areaname`, `parentid`, `arrparentid`, `child`, `arrchildid`, `listorder`) values('401','大兴区','1','0,1','0','401','401');
-insert into `destoon_area` (`areaid`, `areaname`, `parentid`, `arrparentid`, `child`, `arrchildid`, `listorder`) values('402','昌平区','1','0,1','0','402','402');
-insert into `destoon_area` (`areaid`, `areaname`, `parentid`, `arrparentid`, `child`, `arrchildid`, `listorder`) values('403','平谷区','1','0,1','0','403','403');
+insert into `destoon_area` (`areaid`, `areaname`, `parentid`, `arrparentid`, `child`, `arrchildid`, `listorder`) values('110000','北京','0','0','1','110000,110108,110105,110101,110102,110106,110107,110111,110112,110113,110115,110114,110117,110109,110116,110118,110119','1');
+insert into `destoon_area` (`areaid`, `areaname`, `parentid`, `arrparentid`, `child`, `arrchildid`, `listorder`) values('110108','海淀区','110000','0,110000','0','110108','392');
+insert into `destoon_area` (`areaid`, `areaname`, `parentid`, `arrparentid`, `child`, `arrchildid`, `listorder`) values('110105','朝阳区','110000','0,110000','0','110105','393');
+insert into `destoon_area` (`areaid`, `areaname`, `parentid`, `arrparentid`, `child`, `arrchildid`, `listorder`) values('110101','东城区','110000','0,110000','0','110101','394');
+insert into `destoon_area` (`areaid`, `areaname`, `parentid`, `arrparentid`, `child`, `arrchildid`, `listorder`) values('110102','西城区','110000','0,110000','0','110102','395');
+insert into `destoon_area` (`areaid`, `areaname`, `parentid`, `arrparentid`, `child`, `arrchildid`, `listorder`) values('110106','丰台区','110000','0,110000','0','110106','396');
+insert into `destoon_area` (`areaid`, `areaname`, `parentid`, `arrparentid`, `child`, `arrchildid`, `listorder`) values('110107','石景山区','110000','0,110000','0','110107','397');
+insert into `destoon_area` (`areaid`, `areaname`, `parentid`, `arrparentid`, `child`, `arrchildid`, `listorder`) values('110111','房山区','110000','0,110000','0','110111','398');
+insert into `destoon_area` (`areaid`, `areaname`, `parentid`, `arrparentid`, `child`, `arrchildid`, `listorder`) values('110112','通州区','110000','0,110000','0','110112','399');
+insert into `destoon_area` (`areaid`, `areaname`, `parentid`, `arrparentid`, `child`, `arrchildid`, `listorder`) values('110113','顺义区','110000','0,110000','0','110113','400');
+insert into `destoon_area` (`areaid`, `areaname`, `parentid`, `arrparentid`, `child`, `arrchildid`, `listorder`) values('110115','大兴区','110000','0,110000','0','110115','401');
+insert into `destoon_area` (`areaid`, `areaname`, `parentid`, `arrparentid`, `child`, `arrchildid`, `listorder`) values('110114','昌平区','110000','0,110000','0','110114','402');
+insert into `destoon_area` (`areaid`, `areaname`, `parentid`, `arrparentid`, `child`, `arrchildid`, `listorder`) values('110117','平谷区','110000','0,110000','0','110117','403');
+insert into `destoon_area` (`areaid`, `areaname`, `parentid`, `arrparentid`, `child`, `arrchildid`, `listorder`) values('110109','门头沟区','110000','0,110000','0','110109','65535');
+insert into `destoon_area` (`areaid`, `areaname`, `parentid`, `arrparentid`, `child`, `arrchildid`, `listorder`) values('110116','怀柔区','110000','0,110000','0','110116','65535');
+insert into `destoon_area` (`areaid`, `areaname`, `parentid`, `arrparentid`, `child`, `arrchildid`, `listorder`) values('110118','密云区','110000','0,110000','0','110118','65535');
+insert into `destoon_area` (`areaid`, `areaname`, `parentid`, `arrparentid`, `child`, `arrchildid`, `listorder`) values('110119','延庆区','110000','0,110000','0','110119','65535');
