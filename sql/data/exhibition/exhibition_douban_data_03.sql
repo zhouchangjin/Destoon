aa97642b367205cce95f599a09e8558860947f6a
Administrator <Administrator@192.168.1.105>
2018-03-20 12:55:48 +0800

    new data

---
+++ sql/data/exhibition/exhibition_douban_data_03.sql
@@ -0,0 +1,13 @@
+/*
+SQLyog Community v12.5.0 (64 bit)
+MySQL - 10.1.28-MariaDB 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+insert into `destoon_exhibit_data` (`itemid`, `content`) values('290','<h2>活动须知</h2>\n    <div class=\"wr\">\n        客服咨询：15810885790（微信同手机号）');
+insert into `destoon_exhibit_data` (`itemid`, `content`) values('291','<h2>活动须知</h2>\n    <div class=\"wr\">\n        注意事项<br/>1.	为避免发生意外，请勿穿着长短裙或拖鞋上马，请穿长裤和运动鞋。<br/>2.	为了更好的亲近马匹，您可以带少量胡萝卜在教练指导下喂养马匹。<br/>3.	参观马房须在工作人员陪同下参观。<br/>4.	马术是一项具有魅力而又存在危险的运动，请谨慎选择，本运动建议3至60岁参与，不适宜心脏病，高血压及告知不适宜剧烈运动的人使用。<br/>5.	请提前48小时确定出行时间');
+insert into `destoon_exhibit_data` (`itemid`, `content`) values('292','<h2>活动须知</h2>\n    <div class=\"wr\">\n        请活动当天提前15分钟到尤伦斯当代艺术中心一层大厅，会有老师在此等待大家！');
+insert into `destoon_exhibit_data` (`itemid`, `content`) values('293','<h2>附近热门活动（距离本活动3公里范围内）</h2>\n      \n  <ul class=\"gallery gallery-pic115\">\n      <li>\n        <div class=\"pic\">\n          <a tabindex=\"-1\" href=\"https://www.douban.com/event/30021529/\" title=\"张大千艺术展\">\n            <img src=\"https://img3.doubanio.com/pview/event_poster/large/public/353f1dacecd50c1.jpg\" alt=\"\" />\n          </a>');
+insert into `destoon_exhibit_data` (`itemid`, `content`) values('294','<h2>附近热门活动（距离本活动3公里范围内）</h2>\n      \n  <ul class=\"gallery gallery-pic115\">\n      <li>\n        <div class=\"pic\">\n          <a tabindex=\"-1\" href=\"https://www.douban.com/event/30110940/\" title=\"学院与沙龙——法国国家造型艺术中心 巴黎国立高等美术学院珍藏展\">\n            <img src=\"https://img1.doubanio.com/pview/event_poster/large/public/48a7f7055b7cf57.jpg\" alt=\"\" />\n          </a>');
+insert into `destoon_exhibit_data` (`itemid`, `content`) values('295','<h2>附近热门活动（距离本活动3公里范围内）</h2>\n      \n  <ul class=\"gallery gallery-pic115\">\n      <li>\n        <div class=\"pic\">\n          <a tabindex=\"-1\" href=\"https://www.douban.com/event/29224056/\" title=\"【 休闲时间去哪儿】?跟老外来一场高逼格的英语聚会吧~\">\n            <img src=\"https://img1.doubanio.com/pview/event_poster/large/public/001086c9022ee2b.jpg\" alt=\"\" />\n          </a>');
