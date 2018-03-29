d740bafcef613ab2ac16abdcf81f5c469824880f
Administrator <Administrator@192.168.1.105>
2018-03-29 17:49:14 +0800

    new data

---
+++ sql/data/exhibition/exi_douban_data_04.sql
@@ -0,0 +1,14 @@
+/*
+SQLyog Community v12.5.0 (64 bit)
+MySQL - 10.1.28-MariaDB 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+insert into `destoon_exhibit_data` (`itemid`, `content`) values('325','<h2>活动须知</h2>\n    <div class=\"wr\">\n        ·参与人群<br/>适合3—6岁儿童参与，成人陪同<br/><br/>·活动日期<br/>3月24、25、31日、4月1日（周六、日）<br/>时间：下午14:00-18:30<br/><br/>·活动地点（就近）<br/><br/>杨梅红国际私立美校·北京侨福芳草地校区<br/>中国北京朝阳区东大桥路9号侨福芳草地购物中心3层儿童区B301-307<br/><br/>杨梅红国际私立美校·北京蓝色港湾校区<br/>中国北京朝阳区朝阳公园路6号院2号楼（蓝色港湾儿童城）L-VD3-09号店铺<br/><br/>杨梅红国际私立美校·北京望京校区<br/>中国北京朝阳区宜居南路2号楼家乐福购物中心（四元桥店）1层30号<br/><br/>杨梅红国际私立美校·北京金源时代校区<br/>中国北京海淀区远大路1号金源时代购物中心B1层童兜天地021号<br/><br/>·其他说明<br/>报名成功后主办方会联系您');
+insert into `destoon_exhibit_data` (`itemid`, `content`) values('326','<h2>活动须知</h2>\n    <div class=\"wr\">\n        【活动场地】圣恩英语校区<br/>【活动时间】第一场4月01日 15：00-17：00<br/>                      第二场4月15日 15：00-17：00<br/>                      第三场4月22日 15：00-17：00');
+insert into `destoon_exhibit_data` (`itemid`, `content`) values('327','<h2>附近热门活动（距离本活动3公里范围内）</h2>\n      \n  <ul class=\"gallery gallery-pic115\">\n      <li>\n        <div class=\"pic\">\n          <a tabindex=\"-1\" href=\"https://www.douban.com/event/30110940/\" title=\"学院与沙龙——法国国家造型艺术中心 巴黎国立高等美术学院珍藏展\">\n            <img src=\"https://img1.doubanio.com/pview/event_poster/large/public/48a7f7055b7cf57.jpg\" alt=\"\" />\n          </a>');
+insert into `destoon_exhibit_data` (`itemid`, `content`) values('328','<h2>活动须知</h2>\n    <div class=\"wr\">\n        活动时间：2018年3月31日 9:30-11:30<br/><br/>集合地点：日坛公园南门内旗杆下');
+insert into `destoon_exhibit_data` (`itemid`, `content`) values('329','<h2>活动须知</h2>\n    <div class=\"wr\">\n        客服咨询：15810885790（微信同手机号）关注微信公众号，搜索：亲子坐标，有很多有趣的亲子活动资讯哦！~');
+insert into `destoon_exhibit_data` (`itemid`, `content`) values('330','<h2>附近热门活动（距离本活动3公里范围内）</h2>\n      \n  <ul class=\"gallery gallery-pic115\">\n      <li>\n        <div class=\"pic\">\n          <a tabindex=\"-1\" href=\"https://www.douban.com/event/29224056/\" title=\"【 休闲时间去哪儿】?跟老外来一场高逼格的英语聚会吧~\">\n            <img src=\"https://img1.doubanio.com/pview/event_poster/large/public/001086c9022ee2b.jpg\" alt=\"\" />\n          </a>');
+insert into `destoon_exhibit_data` (`itemid`, `content`) values('331','<h2>附近热门活动（距离本活动3公里范围内）</h2>\n      \n  <ul class=\"gallery gallery-pic115\">\n      <li>\n        <div class=\"pic\">\n          <a tabindex=\"-1\" href=\"https://www.douban.com/event/29262289/\" title=\"北京奥林匹克公园荧光夜跑!\">\n            <img src=\"https://img3.doubanio.com/pview/event_poster/large/public/5ca1363621ad813.jpg\" alt=\"\" />\n          </a>');
