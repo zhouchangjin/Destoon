4d4a80098f353a1e56b03458f7ee6ec7ae73500f
Administrator <Administrator@W7OIRX5AGI0XJUM>
2018-10-13 17:43:45 +0800

    公司模板

--- lang/zh-cn/homepage.inc.php
+++ lang/zh-cn/homepage.inc.php
@@ -6,10 +6,10 @@
 defined('IN_DESTOON') or exit('Access Denied');
 $L['msg_buy_deny'] = '您所在的会员组无权查看采购清单';
 $L['msg_contact_deny'] = '您所在的会员组无权查看联系方式';
-$HMENU = $DMENU = array('公司介绍', '供应产品', '采购清单', '新闻中心', '荣誉资质', '人才招聘', '联系方式', '公司相册', '招商代理', '品牌展示', '公司视频', '友情链接', '商城', '诚信档案');
+$HMENU = $DMENU = array('公司介绍', '培训课程', '机构采购', '新闻中心', '机构资质', '人才招聘', '联系方式', '公司相册', '招商代理', '品牌展示', '公司视频', '友情链接', '商城', '诚信档案');
 $MFILE = array('introduce', 'sell', 'buy', 'news', 'honor', 'job', 'contact', 'photo', 'info', 'brand', 'video', 'link','mall','credit','homepage');
-$HSIDE = array('网站公告', '新闻中心', '产品分类', '联系方式', '站内搜索', '荣誉资质', '友情链接');
+$HSIDE = array('网站公告', '新闻中心', '课程分类', '联系方式', '站内搜索', '荣誉资质', '友情链接');
 $SFILE = array('announce', 'news', 'type', 'contact', 'search', 'honor', 'link');
-$HMAIN = array('推荐产品', '公司介绍', '最新供应', '招商代理', '品牌展示', '公司相册', '公司视频', '热卖商品');
+$HMAIN = array('推荐课程', '公司介绍', '最新课程', '招商代理', '品牌展示', '公司相册', '公司视频', '热卖课程');
 $IFILE = array('elite', 'introduce', 'sell', 'info', 'brand', 'photo', 'video', 'mall');
 ?>
